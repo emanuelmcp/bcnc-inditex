92d5c1c5a52db2373b58e90ae2a72e8219fec907
emanuelmcp <emanuelmcp@gmail.com>
2026-09-10 16:44:53 +0200

    Initial commit

---
+++ src/main/java/io/github/emanuelmcp/bcnc_inditex/BcncInditexApplication.java
@@ -0,0 +1,13 @@
+package io.github.emanuelmcp.bcnc_inditex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcncInditexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BcncInditexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/emanuelmcp/bcnc_inditex/BcncInditexApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.emanuelmcp.bcnc_inditex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcncInditexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
