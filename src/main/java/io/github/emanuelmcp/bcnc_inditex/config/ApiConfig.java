ba1f53ffc49569a2dfc534c8bce8194a46dc039d
emanuelmcp <emanuelmcp@gmail.com>
2026-09-12 19:39:17 +0200

    Fixed prefix

--- src/main/java/io/github/emanuelmcp/bcnc_inditex/config/ApiConfig.java
+++ src/main/java/io/github/emanuelmcp/bcnc_inditex/config/ApiConfig.java
@@ -7,11 +7,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class ApiConfig implements WebMvcConfigurer {
+    private static final String API_PREFIX = "/api";
+    private static final String BASE_PACKAGE = "io.github.emanuelmcp.bcnc_inditex";
+
     @Override
     public void configurePathMatch(PathMatchConfigurer configurer) {
         configurer.addPathPrefix(
-                "/api",
+                API_PREFIX,
                 handlerType -> handlerType.isAnnotationPresent(RestController.class)
+                        && handlerType.getPackageName().startsWith(BASE_PACKAGE)
         );
     }
 }
