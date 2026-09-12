26e11e3fafee291c3c984ffbb0a185d507dfad39
emanuelmcp <emanuelmcp@gmail.com>
2026-09-12 16:15:53 +0200

    Fixed bug on exception handler

--- src/main/java/io/github/emanuelmcp/bcnc_inditex/exception/GlobalExceptionHandler.java
+++ src/main/java/io/github/emanuelmcp/bcnc_inditex/exception/GlobalExceptionHandler.java
@@ -7,12 +7,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.time.LocalDateTime;
 
-@RestController
+@RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(PriceNotFoundException.class)
     public ResponseEntity<UnifiedErrorResponseDto> handlePriceNotFoundException(PriceNotFoundException ex, HttpServletRequest request) {
