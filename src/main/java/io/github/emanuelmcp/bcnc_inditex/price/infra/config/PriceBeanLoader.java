ee572803db4da90672c828d29ef6bc6575f6f0a9
emanuelmcp <emanuelmcp@gmail.com>
2026-09-12 13:50:37 +0200

    Created bean loader to manage DI for domain services

---
+++ src/main/java/io/github/emanuelmcp/bcnc_inditex/price/infra/config/PriceBeanLoader.java
@@ -0,0 +1,23 @@
+package io.github.emanuelmcp.bcnc_inditex.price.infra.config;
+
+import io.github.emanuelmcp.bcnc_inditex.price.application.FindApplicablePriceService;
+import io.github.emanuelmcp.bcnc_inditex.price.domain.port.out.PriceRepository;
+import io.github.emanuelmcp.bcnc_inditex.price.domain.service.PriceResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PriceBeanLoader {
+    @Bean
+    public PriceResolver priceResolver() {
+        return new PriceResolver();
+    }
+
+    @Bean
+    public FindApplicablePriceService findApplicablePriceService(
+            PriceRepository priceRepository,
+            PriceResolver priceResolver
+    ) {
+        return new FindApplicablePriceService(priceRepository, priceResolver);
+    }
+}
