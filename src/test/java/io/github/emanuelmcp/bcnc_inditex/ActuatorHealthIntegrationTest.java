4f7ff512b47949288fbcaff6e36ad108897828ad
emanuelmcp <emanuelmcp@gmail.com>
2026-09-12 21:07:49 +0200

    Added dev and test profiles

--- src/test/java/io/github/emanuelmcp/bcnc_inditex/ActuatorHealthIntegrationTest.java
+++ src/test/java/io/github/emanuelmcp/bcnc_inditex/ActuatorHealthIntegrationTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.is;
@@ -11,6 +12,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ActiveProfiles("test")
 @SpringBootTest
 @AutoConfigureMockMvc
 public class ActuatorHealthIntegrationTest {
--- src/test/java/io/github/emanuelmcp/bcnc_inditex/price/infra/adapters/in/PriceControllerIntegrationTest.java
+++ src/test/java/io/github/emanuelmcp/bcnc_inditex/price/infra/adapters/in/PriceControllerIntegrationTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
@@ -16,6 +17,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ActiveProfiles("test")
 @SpringBootTest
 @AutoConfigureMockMvc
 @DisplayName("GET /api/v1/prices - resolución de la tarifa aplicable")
