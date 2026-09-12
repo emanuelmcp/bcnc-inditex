cdf744eca9b0c68577db5aa1556b25a49a78afbe
emanuelmcp <emanuelmcp@gmail.com>
2026-09-12 13:49:13 +0200

    Created JPA entity

---
+++ src/main/java/io/github/emanuelmcp/bcnc_inditex/price/infra/adapters/out/PriceEntity.java
@@ -0,0 +1,51 @@
+package io.github.emanuelmcp.bcnc_inditex.price.infra.adapters.out;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(
+        name = "PRICES",
+        indexes = @Index(
+                name = "idx-prices_brand_product_dates",
+                columnList = "BRAND_ID, PRODUCT_ID, START_DATE, END_DATE"
+        )
+)
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PriceEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "BRAND_ID", nullable = false)
+    private Integer brandId;
+
+    @Column(name = "START_DATE", nullable = false)
+    private LocalDateTime startDate;
+
+    @Column(name = "END_DATE", nullable = false)
+    private LocalDateTime endDate;
+
+    @Column(name = "PRICE_LIST", nullable = false)
+    private Integer priceList;
+
+    @Column(name = "PRODUCT_ID", nullable = false)
+    private Long productId;
+
+    @Column(name = "PRIORITY", nullable = false)
+    private Integer priority;
+
+    @Column(name = "PRICE", nullable = false, precision = 10, scale = 2)
+    private BigDecimal price;
+
+    @Column(name = "CURR", nullable = false, length = 3)
+    private String currency;
+}
