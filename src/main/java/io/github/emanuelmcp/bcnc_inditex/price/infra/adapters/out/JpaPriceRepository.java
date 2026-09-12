67e673d5dd54f0becc30f70f5d80f8924ccf09c4
emanuelmcp <emanuelmcp@gmail.com>
2026-09-12 13:49:32 +0200

    Created JPA repository

---
+++ src/main/java/io/github/emanuelmcp/bcnc_inditex/price/infra/adapters/out/JpaPriceRepository.java
@@ -0,0 +1,23 @@
+package io.github.emanuelmcp.bcnc_inditex.price.infra.adapters.out;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface JpaPriceRepository extends JpaRepository<PriceEntity, Long> {
+    @Query("""
+            SELECT p FROM PriceEntity p
+            WHERE p.brandId = :brandId
+              AND p.productId = :productId
+              AND p.startDate <= :applicationDate
+              AND p.endDate >= :applicationDate
+            """)
+    List<PriceEntity> findCandidates(
+            @Param("brandId") Integer brandId,
+            @Param("productId") Long productId,
+            @Param("applicationDate") LocalDateTime applicationDate
+    );
+}
