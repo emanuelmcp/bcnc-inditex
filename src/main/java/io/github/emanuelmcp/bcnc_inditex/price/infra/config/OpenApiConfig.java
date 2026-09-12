b744f15e35e084bc7c2589b106aa4bdbb35bc421
emanuelmcp <emanuelmcp@gmail.com>
2026-09-12 15:18:54 +0200

    Configured openapi title

---
+++ src/main/java/io/github/emanuelmcp/bcnc_inditex/price/infra/config/OpenApiConfig.java
@@ -0,0 +1,22 @@
+package io.github.emanuelmcp.bcnc_inditex.price.infra.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI pricesOpenApi() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("BCNC Inditex - Prices API")
+                        .description(
+                                """
+                                REST service that resolves the applicable price rate \
+                                for a product of a brand at a given date and time.
+                                """
+                        ).version("v1.0"));
+    }
+}
